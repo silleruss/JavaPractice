d961fbd9fed0e5a90c1884a53dde69ebba1a72b5
silleruss <silleruss@gmail.com>
2020-02-13 10:21:07 +0900

    test commit

--- Test/JustTest.java
+++ Test/JustTest.java
@@ -3,6 +3,13 @@ public class JustTest {
     
     
     public static void main(String[] args) {
-        String s = "";
+        String str = "campaign_promotion_type";
+        
+        char[] arr = str.toCharArray();
+
+        for(int i : arr) {
+            System.out.println(i);
+        }
+
     }
 }
